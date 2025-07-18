431b3fb66cbac9fddcfd646ab02d42928b0ed056
WT <k024g1006@g.neec.ac.jp>
2025-07-18 15:07:40 +0900

    save

--- ClearScene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IScene.h"
-
-class ClearScene : public IScene {
-public:
-	void Init()override;
-	void Update(char* preKeys, char* keys)override;
-	void Draw()override;
-};
--- StageScene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IScene.h"
-
-class StageScene : public IScene {
-public:
-	void Init()override;
-	void Update(char* preKeys, char* keys)override;
-	void Draw()override;
-};
